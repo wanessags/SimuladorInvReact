cf32cb9c33c5ef40e5d49be3b0957b6ea194c800
Wanessa Gonçalves <gwanessags@gmail.com>
2026-09-23 14:58:27 -0300

    feat: display first year investment results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,36 @@
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
+
 export default function Results({ input }) {
-  console.log(input);
-  return <p className="center">Results...</p>;
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{resultsData[0].year}</td>
+          <td>{formatter.format(resultsData[0].valueEndOfYear)}</td>
+          <td>{formatter.format(resultsData[0].interest)}</td>
+          <td>{formatter.format(resultsData[0].interest)}</td>
+          <td>
+            {formatter.format(
+              initialInvestment + resultsData[0].annualInvestment,
+            )}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
